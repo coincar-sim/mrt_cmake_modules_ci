84ba89fb1a022eb14bb8cde076cc02c55ede4b17
Maximilian Naumann <maximilian.naumann@kit.edu>
2018-06-13 13:49:45 +0200

    Change to mrt_cmake_modules_ci

--- README.md
+++ README.md
@@ -1,26 +1,14 @@
-# MoveIt Continous Integration
-Common Travis CI configuration for MoveIt! project
+[![Build Status](https://api.travis-ci.org/coincar-sim/mrt_cmake_modules_ci.svg?branch=master)](https://travis-ci.org/coincar-sim/mrt_cmake_modules_ci)
 
-Authors: Dave Coleman, Isaac I. Y. Saito, Robert Haschke
-
-- Uses Docker for all Distros
-  - Travis does not currently support Ubuntu 16.04
-  - Based on OSRF's pre-build ROS Docker container to save setup time
-  - Uses MoveIt's pre-build Docker container to additionally save setup time
-- Simple - only contains features needed for MoveIt!
-- Clean Travis log files - looks similiar to a regular .travis.yml file
-- Runs tests for the current repo, e.g. if testing moveit\_core only runs tests for moveit\_core
-- Builds into install space
-- Prevents Travis from timing out and from running out of log space, even for huge builds (all of MoveIt!)
-
-[![Build Status](https://travis-ci.org/ros-planning/moveit_ci.svg?branch=master)](https://travis-ci.org/ros-planning/moveit_ci)
+# MRT CMAKE MODULES Continous Integration
+Inspired by and forked from https://travis-ci.org/ros-planning/moveit_ci
 
 ## Usage
 
 Create a ``.travis.yml`` file in the base of you repo similar to:
 
 ```
-# This config file for Travis CI utilizes https://github.com/ros-planning/moveit_ci/ package.
+# This config file for Travis CI utilizes https://github.com/coincar-sim/mrt_cmake_modules_ci/ package.
 sudo: required
 dist: trusty
 services:
@@ -34,68 +22,18 @@ notifications:
     recipients:
       # - user@email.com
 env:
-  matrix:
-    - ROS_DISTRO=kinetic  ROS_REPO=ros  TEST=clang-format
-    - ROS_DISTRO=kinetic  ROS_REPO=ros              UPSTREAM_WORKSPACE=https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit.rosinstall
-    - ROS_DISTRO=kinetic  ROS_REPO=ros-shadow-fixed UPSTREAM_WORKSPACE=https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit.rosinstall
-
-matrix:
-  allow_failures:
-    - env: ROS_DISTRO=kinetic  ROS_REPO=ros              UPSTREAM_WORKSPACE=https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit.rosinstall
+  global:
+    - ROS_DISTRO=kinetic
+    - DEPENDENCIES_ROSINSTALL=dependencies.rosinstall
 
 before_script:
-  - git clone -q https://github.com/ros-planning/moveit_ci.git .moveit_ci
+  - git clone -q https://github.com/coincar-sim/mrt_cmake_modules_ci.git .mrt_cmake_modules_ci
 script:
-  - .moveit_ci/travis.sh
+  - .mrt_cmake_modules_ci/travis.sh
 ```
 
 ## Configurations
 
-- ROS_DISTRO: (required) which version of ROS i.e. kinetic
-- ROS_REPO: (default: ros-shadow-fixed) install ROS debians from either regular release or from shadow-fixed, i.e. http://packages.ros.org/ros-shadow-fixed/ubuntu
-- BEFORE_SCRIPT: (default: not set): Used to specify shell commands or scripts that run before building packages.
-- UPSTREAM_WORKSPACE (default: debian): When set as "file", the dependended packages that need to be built from source are downloaded based on a .rosinstall file in your repository. When set to a "http" URL, this downloads the rosinstall configuration from an http location. Multiple http entries can be given by separating each by comma, but the order of the entries matters -- if there's same resource defined in multiple entries, the one that appears the first is used.
-- TEST_BLACKLIST: Allow certain tests to be skipped if necessary (not recommended)
-- TEST: allow other tests to be run, such as code format checking using clang-format
-
-More configurations as seen in [industrial_ci](https://github.com/ros-industrial/industrial_ci) can be added in the future.
-
-## Removed Configuration
-
-- ROS\_REPOSITORY\_PATH replaced by ROS\_REPO
-
-## Clang-Format
-
-A new test is available that checks if the code is properly formatted as specified in the clang-format file found in ``.clang-format``. Use ``TEST=clang-format`` to enable this test.
-
-## Running Locally For Testing
-
-To manually run the moveit_ci script without Travis (presumably for testing), we will demonstrate with an example using the full moveit repo.
-
-First clone the repo you want to test:
-
-    cd ~/
-    git clone https://github.com/ros-planning/moveit
-    cd moveit
-
-Next clone the CI script:
-
-    git clone https://github.com/ros-planning/moveit_ci .moveit_ci
-
-Manually define the necessary environmental variables:
-
-    export TRAVIS_BRANCH=kinetic-devel
-    export ROS_DISTRO=kinetic
-    export ROS_REPO=ros-shadow-fixed
-
-Note: the following depends on where your rosinstall file is located:
-
-    export UPSTREAM_WORKSPACE=moveit.rosinstall
-
-Optional: will not run test builds but rather only check formatting:
-
-    export TEST=clang-format
-
-Start the script
-
-    .moveit_ci/travis.sh
+- ROS_DISTRO: which version of ROS (e.g. kinetic, note that ubuntu:xenial is used as OS)
+- DEPENDENCIES_ROSINSTALL (optional): rosinstall file pointing to source dependencies
+- other dependencies are resolved via rosdep and [mrt_cmake_modules](https://github.com/KIT-MRT/mrt_cmake_modules)' AutoDeps
--- travis.sh
+++ travis.sh
@@ -5,83 +5,76 @@
 # Inspired by MoveIt! travis https://github.com/ros-planning/moveit_core/blob/09bbc196dd4388ac8d81171620c239673b624cc4/.travis.yml
 # Inspired by JSK travis https://github.com/jsk-ros-pkg/jsk_travis
 # Inspired by ROS Industrial https://github.com/ros-industrial/industrial_ci
+# Inspired by and forked from MoveIt CI https://travis-ci.org/ros-planning/moveit_ci
 #
-# Author:  Dave Coleman, Isaac I. Y. Saito, Robert Haschke
+# Author:  Maximilian Naumann
 
-# Note: ROS_REPOSITORY_PATH is no longer a valid option, use ROS_REPO. See README.md
-
+echo "--- begin of travis.sh ---"
 export CI_SOURCE_PATH=$(pwd) # The repository code in this pull request that we are testing
-export CI_PARENT_DIR=.moveit_ci  # This is the folder name that is used in downstream repositories in order to point to this repo.
+export CI_PARENT_DIR=.mrt_cmake_modules_ci  # This is the folder name that is used in downstream repositories in order to point to this repo.
+export CI_DESKTOP="xenial"
 export HIT_ENDOFSCRIPT=false
 export REPOSITORY_NAME=${PWD##*/}
-export CATKIN_WS=/root/ws_moveit
-echo "---"
-echo "Testing branch '$TRAVIS_BRANCH' of '$REPOSITORY_NAME' on ROS '$ROS_DISTRO'"
+export CATKIN_WS=/root/catkin_ws
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
 
 # Helper functions
 source ${CI_SOURCE_PATH}/$CI_PARENT_DIR/util.sh
 
-# Run all CI in a Docker container
-if ! [ "$IN_DOCKER" ]; then
+# Run all CI in a Docker container -> start docker if not yet inside
+if ! [ -f /.dockerenv ]; then
 
     # Choose the correct CI container to use
-    case "$ROS_REPO" in
-        ros-shadow-fixed)
-            export DOCKER_IMAGE=moveit/moveit:$ROS_DISTRO-ci-shadow-fixed
-            ;;
-        *)
-            export DOCKER_IMAGE=moveit/moveit:$ROS_DISTRO-ci
-            ;;
-    esac
-    echo "Starting Docker image: $DOCKER_IMAGE"
+    . /etc/os-release
+    export DOCKER_IMAGE=ubuntu:xenial
 
     # Pull first to allow us to hide console output
     docker pull $DOCKER_IMAGE > /dev/null
 
     # Start Docker container
+    echo "Starting docker \"$DOCKER_IMAGE\" from a travis machine running \"$PRETTY_NAME\" and run travis.sh inside the docker."
     docker run \
-        -e ROS_REPO \
         -e ROS_DISTRO \
         -e BEFORE_SCRIPT \
         -e CI_PARENT_DIR \
         -e CI_SOURCE_PATH \
-        -e UPSTREAM_WORKSPACE \
+        -e DEPENDENCIES_ROSINSTALL \
         -e TRAVIS_BRANCH \
-        -e TEST \
-        -e TEST_BLACKLIST \
         -v $(pwd):/root/$REPOSITORY_NAME \
         -v $HOME/.ccache:/root/.ccache \
         $DOCKER_IMAGE \
-        /bin/bash -c "cd /root/$REPOSITORY_NAME; source .moveit_ci/travis.sh;"
+        /bin/bash -c "cd /root/$REPOSITORY_NAME; source .mrt_cmake_modules_ci/travis.sh;"
     return_value=$?
 
     if [ $return_value -eq 0 ]; then
-        echo "$DOCKER_IMAGE container finished successfully"
+        echo "Docker \"$DOCKER_IMAGE\" finished successfully"
         HIT_ENDOFSCRIPT=true;
         exit 0
     fi
-    echo "$DOCKER_IMAGE container finished with errors"
+    echo "Docker \"$DOCKER_IMAGE\" finished with errors"
     exit 1 # error
 fi
 
 # If we are here, we can assume we are inside a Docker container
-echo "Inside Docker container"
+. /etc/os-release
+echo "Inside docker container, running \"$PRETTY_NAME\""
+echo "Testing branch '$TRAVIS_BRANCH' of '$REPOSITORY_NAME' on ROS '$ROS_DISTRO'"
+
+# Adding ros repo
+echo "Adding ros repo to apt sources"
+sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $CI_DESKTOP main\" > /etc/apt/sources.list.d/ros-latest.list"
+travis_run apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 # Update the sources
 travis_run apt-get -qq update
 
-# Make sure the packages are up-to-date
-travis_run apt-get -qq dist-upgrade
-
-# Split for different tests
-for t in $TEST; do
-    case "$t" in
-        "clang-format")
-            source ${CI_SOURCE_PATH}/$CI_PARENT_DIR/check_clang_format.sh || exit 1
-            exit 0 # This runs as an independent job, do not run regular travis test
-        ;;
-    esac
-done
+# Install the required apt packages
+travis_run apt-get install -y build-essential
+travis_run apt-get install -y python-catkin-pkg
+travis_run apt-get install -y python-rosdep
+travis_run apt-get install -y python-wstool
+travis_run apt-get install -y ros-$ROS_DISTRO-catkin
+travis_run apt-get install -y python-catkin-tools
 
 # Enable ccache
 travis_run apt-get -qq install ccache
@@ -93,101 +86,46 @@ Xvfb -screen 0 640x480x24 :99 &
 export DISPLAY=:99.0
 travis_run_true glxinfo
 
-# Setup rosdep - note: "rosdep init" is already setup in base ROS Docker image
+# Setup rosdep and add rosdep-definitions from mrt_cmake_modules
+travis_run rosdep init
+travis_run echo "Adding rosdep-definitions from mrt_cmake_modules"
+echo "yaml https://raw.githubusercontent.com/KIT-MRT/mrt_cmake_modules/master/yaml/base.yaml" | tee --append /etc/ros/rosdep/sources.list.d/20-default.list
 travis_run rosdep update
 
+# Source ROS
+travis_run source /opt/ros/$ROS_DISTRO/setup.bash
+
 # Create workspace
-travis_run mkdir -p $CATKIN_WS/src
-travis_run cd $CATKIN_WS/src
-
-# Install dependencies necessary to run build using .rosinstall files
-if [ ! "$UPSTREAM_WORKSPACE" ]; then
-    export UPSTREAM_WORKSPACE="debian";
-fi
-case "$UPSTREAM_WORKSPACE" in
-    debian)
-        echo "Obtain deb binary for upstream packages."
-        ;;
-    http://* | https://*) # When UPSTREAM_WORKSPACE is an http url, use it directly
-        travis_run wstool init .
-        # Handle multiple rosintall entries.
-        IFS=','  # Multiple URLs can be given separated by comma.
-        for rosinstall in $UPSTREAM_WORKSPACE; do
-            travis_run wstool merge -k $rosinstall
-        done
-        ;;
-    *) # Otherwise assume UPSTREAM_WORKSPACE is a local file path
-        travis_run wstool init .
-        if [ -e $CI_SOURCE_PATH/$UPSTREAM_WORKSPACE ]; then
-            # install (maybe unreleased version) dependencies from source
-            travis_run wstool merge file://$CI_SOURCE_PATH/$UPSTREAM_WORKSPACE
-        else
-            echo "No rosinstall file found, aborting" && exit 1
-        fi
-        ;;
-esac
-
-# download upstream packages into workspace
-if [ -e .rosinstall ]; then
-    # ensure that the downstream is not in .rosinstall
-    # the exclamation mark means to ignore errors
-    travis_run_true wstool rm $REPOSITORY_NAME
-    travis_run cat .rosinstall
-    travis_run wstool update
-fi
-
-# link in the repo we are testing
+travis_run mkdir -p $CATKIN_WS
+travis_run cd $CATKIN_WS
+travis_run wstool init src
+travis_run catkin init
+# Configure catkin to build in debug mode
+travis_run catkin config --cmake-args -DCMAKE_BUILD_TYPE=Debug
+# Add the package under integration to the workspace using a symlink.
+travis_run cd ~/catkin_ws/src
 travis_run ln -s $CI_SOURCE_PATH .
 
-# Debug: see the files in current folder
-travis_run ls -a
-
-# Run before script
-if [ "${BEFORE_SCRIPT// }" != "" ]; then
-    travis_run sh -c "${BEFORE_SCRIPT}";
+# Install all dependencies
+# Source dependencies: install using wstool.
+travis_run cd ~/catkin_ws
+if [[ -f "$CI_SOURCE_PATH/$DEPENDENCIES_ROSINSTALL" ]] ; then
+    travis_run wstool merge -t src "$CI_SOURCE_PATH/$DEPENDENCIES_ROSINSTALL"
+    travis_run wstool update -t src ;
+else
+    echo "No rosinstall file provided, was looking for $CI_SOURCE_PATH/$DEPENDENCIES_ROSINSTALL" ;
 fi
 
-# Install source-based package dependencies
-travis_run rosdep install -y -q -n --from-paths . --ignore-src --rosdistro $ROS_DISTRO
+# Package depdencies: install using rosdep.
+travis_run rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 
-# Change to base of workspace
-travis_run cd $CATKIN_WS
-
-# Configure catkin
-travis_run catkin config --extend /opt/ros/$ROS_DISTRO --install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3"
-
-# Console output fix for: "WARNING: Could not encode unicode characters"
-export PYTHONIOENCODING=UTF-8
-
-# For a command that doesn’t produce output for more than 10 minutes, prefix it with travis_run_wait
-travis_run_wait 60 catkin build --no-status --summarize || exit 1
-
-travis_run ccache -s
-
-# Source the new built workspace
-travis_run source install/setup.bash;
-
-# Choose which packages to run tests on
-echo "Test blacklist: $TEST_BLACKLIST"
-echo "--------------"
-TEST_PKGS=$(catkin_topological_order "$CI_SOURCE_PATH" --only-names | grep -Fvxf <(echo "$TEST_BLACKLIST" | tr ' ;,' '\n') | tr '\n' ' ')
-
-if [ -n "$TEST_PKGS" ]; then
-    TEST_PKGS="--no-deps $TEST_PKGS";
-    # Fix formatting of list of packages to work correctly with Travis
-    IFS=' ' read -r -a TEST_PKGS <<< "$TEST_PKGS"
+# Build and test
+travis_run cd ~/catkin_ws
+travis_run catkin build --no-status --continue-on-failure
+# Run the tests and check the results. (But do not test this (mrt_cmake_modules_ci) package as it would fail.)
+if [[ $REPOSITORY_NAME != mrt_cmake_modules_ci ]] ; then
+    travis_run catkin run_tests --no-status $REPOSITORY_NAME --no-deps
+    travis_run catkin_test_results --verbose build/$REPOSITORY_NAME ;
+else
+    echo "Not testing this ci-package (mrt_cmake_modules_ci) as it is not a catkin package itself" ;
 fi
-echo -e "Test packages: ${TEST_PKGS}"
-
-# Run catkin package tests
-travis_run catkin build --no-status --summarize --make-args tests -- ${TEST_PKGS[@]}
-
-# Run non-catkin package tests
-travis_run catkin build --catkin-make-args run_tests -- --no-status --summarize ${TEST_PKGS[@]}
-
-# Show test results and throw error if necessary
-travis_run catkin_test_results
-
-echo "Travis script has finished successfully"
-HIT_ENDOFSCRIPT=true
-exit 0
